b77d880746178bc79cea94eb8522ca8ae977d310
ryan <ryanwaszak@gmail.com>
2020-04-13 20:05:51 -0500

    add Login.vue, Logout.vue, and Signup.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
   },
+
+  { 
+    path: "/signup",
+    name: "signup",
+    component: Signup 
+  },
+
+  { 
+    path: "/login",
+    name: "login",
+    component: Login 
+  },
+
+  { path: "/logout",
+    name: "logout",
+    component: Logout 
+  },  
+
   {
     path: '/about',
     name: 'About',
