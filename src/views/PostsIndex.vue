0d0f440eafc2af164d07badc8d97d0445bcbdf0f
ryan <ryanwaszak@gmail.com>
2020-04-19 12:28:54 -0500

    install bootstrap via npm, add navbar, add bootstrap cards to /posts

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -2,10 +2,18 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <div v-for="post in posts">
-      <p>id: {{ post.id }}</p>
+      <!-- <p>id: {{ post.id }}</p>
       <p>title: <a v-bind:href="`/posts/${post.id}`">{{ post.title }}</a></p>
       <p>body: {{ post.body }}</p>
-      <img v-bind:src="post.image">
+      <img v-bind:src="post.image"> -->
+      <div class="card" style="width: 28rem;">
+        <img v-bind:src="post.image">
+        <div class="card-body">
+          <h5 class="card-title">Title: <a v-bind:href="`/posts/${post.id}`">{{ post.title }}</a></h5>
+          <p class="card-text">Body: {{ post.body }}</p>
+          <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+        </div>
+      </div>
       <hr>
       <hr>
     </div>
